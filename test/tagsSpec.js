b353f1604d7aa0f1f1e6900c55c4fa94b0c702b5
Matt Travi <programmer@travi.org>
2014-08-22 17:40:29 -0500

    add default option

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -6,17 +6,26 @@ var assert = require("chai").assert,
     sinonChai = require("sinon-chai");
 
 describe("Tags", function(){
-    describe("#parse()", function(){
+    describe("#parse()", function () {
+        var anyDepth = randomNumber.integer(0, 9)(randomNumber.engines.nativeMath),
+            world = randomString.generate(),
+            args = [("--depth=" + anyDepth), ("--hello=" + world)];
+
         it("should parse long-formed tags", function(){
-            var anyDepth = randomNumber.integer(0, 9)(randomNumber.engines.nativeMath),
-                world = randomString.generate(),
-                results;
+            var results = tags.parse(args);
 
-            results = tags.parse([("--depth=" + anyDepth), ("--hello=" + world)]);
+            assert.deepEqual(results.depth, anyDepth);
+            assert.deepEqual(results.hello, world);
+        });
 
-            assert.equal(results.depth, anyDepth);
-            assert.equal(results.hello, world);
+        it("should fallback to defaults", function () {
+            var results = tags.parse(args);
+
+            assert.deepEqual({
+                depth: anyDepth,
+                hello: world,
+                foo: 'bar'
+            }, results);
         });
      });
-
 });
